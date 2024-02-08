f176d9cf70d2e525b681d941694d22f95d6e04cf
Ishmum <ishmum20000@gmail.com>
2024-02-08 21:54:42 +0600

    Set up the component boilerplates

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from './Billing/Billing.jsx';
+import Business from './Business/Business.jsx';
+import Button from './Button/Button.jsx';
+import CardDeal from './CardDeal/CardDeal.jsx';
+import Clients from './Clients/Clients.jsx';
+import CTA from './CTA/CTA.jsx';
+import FeedbackCard from './FeedbackCard/FeedbackCard.jsx';
+import Footer from './Footer/Footer.jsx';
+import GetStarted from './GetStarted/GetStarted.jsx';
+import Hero from './Hero/Hero.jsx';
+import Navbar from './Navbar/Navbar.jsx';
+import Stats from './Stats/Stats.jsx';
+import Testimonials from './Testimonials/Testimonials.jsx';
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Button,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Stats,
+    Footer,
+    GetStarted,
+    Testimonials,
+    Hero,
+  };
